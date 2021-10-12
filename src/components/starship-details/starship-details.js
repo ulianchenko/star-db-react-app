6a97f0ddce77381cc744df136062dcc46588837f
Ulianchenko <ulianchenko@gmail.com>
2021-10-12 16:26:57 +0300

    the app sructure was made

---
+++ src/components/starship-details/starship-details.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class StarshipDetails extends React.Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+};
+
+export default StarshipDetails;
